035d03c21abdc2da008b910511e497c7aa465321
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-08-27 03:10:28 +0600

    product api connect and fetch data

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import {useState,useEffect} from "react";
+import axios from "axios";
+import Image from '../Image';
 
 const About = () => {
+
+  let [allData,setAllData]=useState([]);
+
+  useEffect(() => {
+     
+    async function viewData(){
+      let data =await axios.get("https://abdunnoorfaheem.github.io/apiProductData/data/index.json");  
+
+      setAllData(data.data.data);
+      
+    }
+    viewData();
+  }, []);
+
+  
+  
   return (
     <>
-      <h1>About</h1>
+      <h1>data</h1>
+      {
+        allData.map((item)=>(
+          <div className="" key={item.id}>
+            <img src={`https://abdunnoorfaheem.github.io/apiProductData/${item.product_image.url}`} alt="" />
+            <h3>{item.title}</h3>
+            <h4>{item.price}</h4>
+            
+          </div>
+        ))
+      }
     </>
   )
 }
